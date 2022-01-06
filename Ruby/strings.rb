cadb303b803547c5c99404abe285815f250a8a06
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-01-06 13:41:13 -0500

    strings

---
+++ Ruby/strings.rb
@@ -0,0 +1,14 @@
+# String concatentation
+first_name = "Sai"
+last_name = "Emani"
+
+full_name = first_name + " " + last_name
+puts full_name
+
+# String interpolation
+full_name = "My name is #{first_name} #{last_name}"
+puts full_name
+
+# Escaping
+message = "I said \'Hello World\'"
+puts message
